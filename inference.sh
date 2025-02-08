a8a0fbe35931beb2ebe4c5420b5e47f8f9c40748
jigyanshrathore <122160447+jigyanshrathore@users.noreply.github.com>
2025-02-09 00:24:24 +0530

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# Default value for super-resolution
+SUPERRES_METHOD="none"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --superres)
+            SUPERRES_METHOD="$2"
+            shift ;;
+        *) 
+            
+            ;;
+    esac
+    shift
+done
+
 python -m scripts.inference \
     --unet_config_path "configs/unet/second_stage.yaml" \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 20 \
     --guidance_scale 1.5 \
-    --video_path "assets/demo1_video.mp4" \
-    --audio_path "assets/demo1_audio.wav" \
-    --video_out_path "video_out.mp4"
+    --video_path "assets/input_video_with_audio.mp4" \
+    --audio_path "assets/input_audio.wav" \
+    --video_out_path "video_out.mp4" \
+    --superres "${SUPERRES_METHOD}"
